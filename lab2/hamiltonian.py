be51cf53d27c195925dda086e6560709260a8a7a
Wer1d <98684652+Wer1d@users.noreply.github.com>
2023-08-24 18:54:37 +0700

    added comment

--- lab2/hamiltonian.py
+++ lab2/hamiltonian.py
@@ -7,34 +7,41 @@
 #              4. Hamiltonian path = path ที่ต้องผ่านทุก vertexเพียง 1 ครั้ง
  
 # steps : 1. ต้องมีตัวเช็คว่า visite vertex ไปยัง
-#         2. เริ่มจาก vertex 0 แล้วเช็คว่า visited อันไหนบ้าง ถ้าไปได้ append to path 
+#         2. เริ่มจาก vertex 0 แล้วเช็คว่า visited อันไหนบ้าง ถ้าไปได้ append to path
 
 class Graph:
+
+    # constructor like Java
     def __init__(self, matrix):
         self.matrix = matrix
-        self.visited = [0 for i in range(len(matrix))]
+        self.visited = [0 for i in range(len(matrix))] # 0 = not visited, 1 = visited
         self.path = []
 
-    def findPathFrom(self, u, v, current_path=[]):  # Pass the current path as an argument
-        self.visited[u] = 1
-        current_path.append(u)
+    # find single path(u,v) แบบ DFS --> เก็บ path แบบวิ่งลงเรื่อยๆ เลยต้อง implement stack 
+    def findPathFrom(self, u, v, current_path=[]):  
+        self.visited[u] = 1 # set visited at index = u(start index) to 1 (visited)
+        current_path.append(u) # add u vertex u to current_path
         if u == v:
-            self.path.append(current_path.copy())
+            self.path.append(current_path.copy()) 
+            # use .copy() ทำให้ไปเก็บที่ memory ใหม่ ทำให้ค่า immutable แก้ไขไม่ได้ ถ้าไม่ใช้ .copy() จะเป็นการเก็บที่ memory เดิม ทำให้เป็น mutable แก้ไขได้
         else:
-            for available_node, is_connected in enumerate(self.matrix[u]):
-                if is_connected and not self.visited[available_node]:
-                    # Pass the current_path to the recursive call
-                    self.findPathFrom(available_node, v, current_path)
+            for available_node, is_connected in enumerate(self.matrix[u]): # index,value = enumerate()
+                if is_connected > 0 and not self.visited[available_node]: # ถ้า is_connected = 1(value from matrix) และ vertex นั้นยังไม่เคยถูกเดิน
+                    self.findPathFrom(available_node, v, current_path) 
+                    # ให้เดินไปยัง vertex ถัดไป start vertex รอบนี้เป็น available_node และมี current_path เป็น knowledge ส่งไป
+                    # recursive call เหมือนเดินไปเรื่อยๆ จนกว่าจะเจอ available_node = v จะ append current_path ทุกอันที่เจอเข้าไปใน path
+        self.visited[u] = 0 # set back visited at index = u(start index) to 0 (not visited) ทำให้เริ่มต้น path ใหม่ได้
+        # print("current_path before pop: ", current_path)
+        current_path.pop() # backtracking
 
-        self.visited[u] = 0
-        current_path.pop()
-
+    # find all path from u to v
     def findAllPathFrom(self, u, v):
         self.path = []
         self.findPathFrom(u, v)
         self.visited = [0 for i in range(len(self.matrix))]
         return self.path
     
+    # find all path via brute force -> หา path(0,0) path(0,1),...,path(n,n)
     def findAllPath(self):
         self.path = []
         allPath = []
@@ -43,9 +50,10 @@ class Graph:
                 self.findPathFrom(i, j)
         allPath.append(self.path.copy())
             
-        self.visited = [0 for i in range(len(self.matrix))]
+        self.visited = [0 for i in range(len(self.matrix))] # set visited to 0 again ทำให้เรียกใช้ method หลายครั้งหรือใช้อันอื่นได้
         return allPath
     
+    # เลือกจาก all path เอาตัวที่ มีจำนวน vertex เท่ากับจำนวน vertex ทั้งหมด
     def findHamiltonianPath(self):
         allPath = self.findAllPath()
         hamilPath = []
@@ -56,13 +64,14 @@ class Graph:
                 hamilPath.append(path)
         return hamilPath
 
+    # hamiltonian path มาเช็คว่าตำแหน่ง start กับ end มี edge ไหม ถ้ามี adjacency[start][end] = 1
     def findHamiltonianCycle(self): 
         allHamiltonianPath = self.findHamiltonianPath()
         hamilCycle = []
         for hamilPath in allHamiltonianPath:
                 start =  hamilPath[0] 
                 stop = hamilPath[-1]
-                if self.matrix[start][stop] == 1:
+                if self.matrix[start][stop] > 0 :
                     hamilCycle.append(hamilPath)
         return hamilCycle
     
